18a2bd314068be970836b771930e15b0e1549b31
ferney <williamchaparro@unisangil.edu.co>
2017-05-06 11:03:26 -0500

    Update fecha.java

--- mogotes_ejemplo_POO/src/clases_objetos/fecha.java
+++ mogotes_ejemplo_POO/src/clases_objetos/fecha.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package clases_objetos;
 
-/**
- *
- * @author Admin
- */
+//aqui se evidenciasn el manejo de las palabras reservadas, public, private, protected
 public class fecha {
      protected int dia;
     protected int mes;
